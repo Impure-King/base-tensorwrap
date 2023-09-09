d270a60988d94e155609f778f706776e2ad984d2
Lelouch <25KumarAadesh@gmail.com>
2023-09-09 08:52:48 -0600

    Added an history object

--- tensorwrap/nn/models/train_fun.py
+++ tensorwrap/nn/models/train_fun.py
@@ -1,3 +1,4 @@
+from typing import Any
 import tensorwrap as tw
 from tensorwrap.module import Module
 from tensorwrap.nn import optimizers
@@ -5,7 +6,22 @@ import jax
 import copy
 from termcolor import colored
 
+# Creating a history management class:
+class History(Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.epoch = 1
+        self.dict = {}
 
+    def __call__(self, loss_value, metrics_value, val_loss, val_metrics):
+        epoch_dict = {
+            "loss": loss_value,
+            "metrics": metrics_value,
+            "val_loss": val_loss,
+            "val_metrics": val_metrics
+        }
+        self.dict[self.epoch] = epoch_dict
+        self.epoch += 1
 # Creating a Training Class:
 class Train(Module):
     def __init__(self, model, loss_fn, optimizer, metric_fn, copy_model = True) -> None:
@@ -15,6 +31,7 @@ class Train(Module):
         self.model = copy.deepcopy(model)
         self.optimizer = optimizer
         self.state = self.optimizer.init(self.model.trainable_variables)
+        self.history = History()
 
         @jax.value_and_grad
         def grad_fn(params, X, y):
@@ -22,7 +39,7 @@ class Train(Module):
             return self.loss_fn(y, pred)
         self.grad_fn = grad_fn
     
-    def train(self, X_train, y_train, epochs = 1, batch_size = 32, validation_data = None):
+    def train(self, X_train, y_train, epochs = 1, batch_size = 32, validation_data = None, callbacks = [lambda x: x]):
         X_train_batched = tw.experimental.data.Dataset(X_train).batch(batch_size)
         y_train_batched = tw.experimental.data.Dataset(y_train).batch(batch_size)
         if validation_data is not None:
@@ -32,14 +49,19 @@ class Train(Module):
             val_loss = None
             val_metrics = None
         compiled_update = jax.jit(self.update)
+        self.trainable_variables = [self.model.trainable_variables]
         for epoch in range(1, epochs+1):
             print(f"Epoch {epoch}/{epochs}")
             for index, (X, y) in enumerate(zip(X_train_batched, y_train_batched)): 
                 self.model.trainable_variables, losses, self.state = compiled_update(self.model.trainable_variables, self.state, X, y)
-                accuracy = self.metric_fn(y, self.model(self.model.trainable_variables, X))
+                self.trainable_variables.append(self.model.trainable_variables)
+                metrics = self.metric_fn(y, self.model(self.model.trainable_variables, X))
                 if validation_data is not None:
                     val_loss, val_metrics = compile_val_score(self.model.trainable_variables, X_valid, y_valid)
-                self.loading_animation(X_train_batched.len(), index+1, losses, accuracy, val_loss=val_loss, val_metric=val_metrics)
+                self.loading_animation(X_train_batched.len(), index+1, losses, metrics, val_loss=val_loss, val_metric=val_metrics)
+            self.history(losses, metrics, val_loss, val_metrics)
+            for callback in callbacks:
+                callback(self.history)
             print("\n") 
     
     def return_params(self):
@@ -47,6 +69,9 @@ class Train(Module):
 
     def return_model(self):
         return self.model
+    
+    def return_train_history(self):
+        return self.history
     
     def evaluate(self, X_test, y_test):
         self.model.evaluate(X_test, y_test, self.loss_fn, self.metric_fn)
@@ -75,7 +100,7 @@ class Train(Module):
         if val_metric is None:
             val_met_str = ""
         else:
-            val_met_str = f"    -    val_loss: {val_metric:.5f}"
+            val_met_str = f"    -    val_metrics: {val_metric:.5f}"
         print(f'\rBatch {current_batch}/{total_batches} [{bar}]    -    loss: {loss:.5f}    -    metric: {metric:.5f}' + val_loss_str + val_met_str, end='', flush=True)
 
 # @tw.value_and_grad
